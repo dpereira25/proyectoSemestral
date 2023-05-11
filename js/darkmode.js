133c28a490680abc439725e013dad09b37207517
Benjamom <ben.duranr@duocuc.cl>
2023-05-10 22:36:26 -0400

    DarkMode

---
+++ js/darkmode.js
@@ -0,0 +1,10 @@
+function darkMode() {
+
+    document.querySelector('body').setAttribute("data-bs-theme", "dark");
+    
+}
+function lightMode() {
+
+    document.querySelector('body').setAttribute("data-bs-theme", "light");
+    
+}
